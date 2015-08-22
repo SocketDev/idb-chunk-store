2a7d7be7612b9ed2191ba0760b32b7f1acd8940d
Feross Aboukhadijeh <feross@feross.org>
2015-08-22 17:20:19 +0200

    allow last chunk to be smaller than chunkLength if length is passed

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 module.exports = Storage
 
-function Storage (chunkLength) {
-  if (!(this instanceof Storage)) return new Storage()
-  this.chunks = []
+function Storage (chunkLength, opts) {
+  if (!(this instanceof Storage)) return new Storage(chunkLength, opts)
+  if (!opts) opts = {}
+
   this.chunkLength = Number(chunkLength)
-  this.closed = false
   if (!this.chunkLength) throw new Error('First argument must be a chunk length')
+
+  this.chunks = []
+  this.closed = false
+  this.length = Number(opts.length) || Infinity
+
+  if (this.length !== Infinity) {
+    this.lastChunkLength = (this.length % this.chunkLength) || this.chunkLength
+    this.lastChunkIndex = Math.ceil(this.length / this.chunkLength) - 1
+  }
 }
 
 Storage.prototype.put = function (index, buf, cb) {
   if (this.closed) return nextTick(cb, new Error('Storage is closed'))
-  if (buf.length !== this.chunkLength) {
+
+  var isLastChunk = (index === this.lastChunkIndex)
+  if (isLastChunk && buf.length !== this.lastChunkLength) {
+    return nextTick(cb, new Error('Last chunk length must be ' + this.lastChunkLength))
+  }
+  if (!isLastChunk && buf.length !== this.chunkLength) {
     return nextTick(cb, new Error('Chunk length must be ' + this.chunkLength))
   }
   this.chunks[index] = buf
-  if (cb) process.nextTick(cb)
+  nextTick(cb, null)
 }
 
 Storage.prototype.get = function (index, opts, cb) {
