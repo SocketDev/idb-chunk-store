a0f20fddeb5c235026769c5452799511002f069b
John Hiesey <john@hiesey.com>
2021-06-02 12:50:45 -0700

    Remove bad test

--- test.js
+++ test.js
@@ -3,12 +3,3 @@ const tape = require('tape')
 const idb = require('./')
 
 tests(tape, idb)
-
-tape('empty test case', function (t) {
-  t.plan(2)
-  const store = idb(5, { name: 'test-store-' + Math.random() })
-  store.get(0, function (err, buf) {
-    t.ifError(err)
-    t.deepEqual(buf, Buffer.alloc(0))
-  })
-})
